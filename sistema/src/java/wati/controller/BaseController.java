94b86ae09db0a3eadb06ee221c0a47a7f92f55ef
Heder Bernardino <hedersb@gmail.com>
2013-07-16 23:05:14 -0300

    Inclui infraestrutura para gravar usuários novos.

---
+++ sistema/src/java/wati/controller/BaseController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wati.controller;
+
+import wati.persistence.GenericDAO;
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.Map;
+import java.util.PropertyResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hedersb
+ */
+public abstract class BaseController<T> implements Serializable {
+	
+	@PersistenceContext
+    private EntityManager entityManager = null;
+	
+	private GenericDAO<T> daoBase;
+
+	public BaseController( Class<T> cls ) {
+		
+		try {
+			this.daoBase = new GenericDAO<T>( cls );
+		} catch (NamingException ex) {
+			String message = 
+					PropertyResourceBundle.getBundle( "br.org.bssystem.utility.messages" ).getString( "unexpected_error_message" );
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, message, null));
+			Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+		}
+		
+	}
+
+	/**
+	 * @return the entityManager
+	 */
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	/**
+	 * @param entityManager the entityManager to set
+	 */
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	/**
+	 * @return the daoBase
+	 */
+	public GenericDAO<T> getDaoBase() {
+		return daoBase;
+	}
+	
+	/**
+	 *
+	 * @return ParameterMap
+	 */
+	public Map<String, String> getParameterMap() {
+		return (Map<String, String>) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+	}
+	
+	/**
+	 *
+	 * @return Locale
+	 */
+	protected Locale getLocale( ) {
+		return FacesContext.getCurrentInstance().getApplication().evaluateExpressionGet(FacesContext.getCurrentInstance(), "#{languageBean.language}", Locale.class);
+	}
+	
+}
---
+++ sistema/src/java/wati/model/User.java
@@ -0,0 +1,134 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wati.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author hedersb
+ */
+@Entity
+@Table(name="tb_user")
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="name", length=100)
+    private String name;
+	
+	@Column(name="email", length=50, unique=true)
+    private String email;
+	
+	@Column(name="password", length=16)
+    private byte[] password;
+	
+	@Column(name="birth")
+	@Temporal(javax.persistence.TemporalType.DATE)
+    private Date birth;
+	
+	@Column(name="gender")
+    private char gender;
+	
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public byte[] getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(byte[] password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return this.id + ", " + this.name + ", " + this.email + ", " + new String( this.password );
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the birth
+	 */
+	public Date getBirth() {
+		return birth;
+	}
+
+	/**
+	 * @param birth the birth to set
+	 */
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public char getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+}
