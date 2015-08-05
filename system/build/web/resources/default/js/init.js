444c23d30f126eb6a21112f54182b3921a46f4d7
Henrique Gomide <henriquepgomide@gmail.com>
2015-08-05 11:39:21 -0500

    Added basic layout for internal pages, added language support to top menu

--- system/build/web/resources/default/js/init.js
+++ system/build/web/resources/default/js/init.js
@@ -1,9 +1,24 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $(".dropdown-button").dropdown();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false // Displays dropdown below the button
+        }
+        );
+        $('.collapsible').collapsible();
+        $('ul.tabs').tabs();
+        $(document).ready(function () {
+            $('select').material_select();
+        });
 
-  }); // end of document ready
+
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
