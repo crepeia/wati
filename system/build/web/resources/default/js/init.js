54af079a1ce2790dd4a8cae57d4fa3f8282fde27
Thiago <t.rizuti@gmail.com>
2015-10-07 14:08:29 -0300

    Revert "gitignore updated"
    This reverts commit b635a0d5921a4351e6a98eba3a8b5f0c31efeb7b.

---
+++ system/build/web/resources/default/js/init.js
@@ -0,0 +1,24 @@
+(function ($) {
+    $(function () {
+        $('.button-collapse').sideNav();        
+        $('.parallax').parallax();
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false // Displays dropdown below the button
+        }
+        );
+        $('.collapsible').collapsible();
+        $('ul.tabs').tabs();
+        $(document).ready(function () {
+            $('select').material_select();
+        });
+        $(document).ready(function () {
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal-trigger').leanModal();
+        });        
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
